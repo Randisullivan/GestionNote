f0fc32a2354fe50df7222a6059416ab611c5304b
unknown <randisilva19@gmail.com>
2023-08-12 15:13:25 +0200

    first commit

--- GestionNotes/settings.py
+++ GestionNotes/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,7 +125,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'authentification/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'authentification/static/')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
